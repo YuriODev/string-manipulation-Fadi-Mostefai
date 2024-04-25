6ddc73e59a788953dbdeed7d7a8ae335f9ea25dc
Fadi <130671609+Fadi-Mostefai@users.noreply.github.com>
2024-04-25 12:16:17 +0000

    Completed Exercise 11

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 11
+string = str(input())
+index_store = 0
+for index in range(len(string) - 1, 1, -1):
+  if string[index] == " ":
+    index_store = index
+    break
+if index_store == 0:
+  print(string[:-1])
+else:
+  print(string[index_store + 1:])
